e937d683818ba75c1dd2782dd7e46103590a8c36
SofiaDeAlessandre <sofia.dealessandre@hotmail.com>
2026-07-13 22:02:52 -0300

    se agregó middleware de roles

---
+++ src/middlewares/roleMiddleware.js
@@ -0,0 +1,16 @@
+const checkRole = (...roles) => {
+  return (req, res, next) => {
+    const userRole = req.userLogged.role;
+
+    if (!roles.includes(userRole)) {
+      return res.status(403).json({
+        success: false,
+        error: "Forbidden: you don't have permission to access this resource",
+      });
+    }
+
+    next();
+  };
+};
+
+export { checkRole };
